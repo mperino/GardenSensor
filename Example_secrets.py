337c4d33fdf573ed3062d3b2e6b4e0b5d5ce2743
mperino <mark.perino@gmail.com>
2021-03-30 07:32:17 -0700

    Add files via upload

---
+++ Example_secrets.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# go to io.adafruit.com and get an account.  
+
+secrets = {
+    'ssid' : 'Insert WIFI NAME',
+    'password' : 'Insert WIFI',
+    'timezone' : "America/Los_Angeles", # http://worldtimeapi.org/timezones
+    'aio_username': 'goto adafruit and sign up an account',
+    'aio_key': 'goto adafruit and sign up an account'
+    }
